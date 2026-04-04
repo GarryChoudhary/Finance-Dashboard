7573a31cc14a3974856713c958e5aeed595c607e
GarryChoudhary <gggaurav2210@gmail.com>
2026-04-05 01:38:03 +0530

    add charts

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let transactions = [];
+    let transactions = JSON.parse(localStorage.getItem("financeData")) || [];
+    let myChart; 
 
-function addTransactions() {
+
+function addTransactions(event) {
+    if (event) event.preventDefault();
     let amount = document.getElementById('add-amount').value;
     let type = document.getElementById('type').value;
     let catagory = document.getElementById('add-catagory').value;
@@ -22,17 +25,8 @@ function addTransactions() {
 
 
     transactions.push(newList);
+    localStorage.setItem("financeData", JSON.stringify(transactions));
 
-
-    let table = document.getElementById("transactionList");
-    let newRow = document.createElement("tr");
-    newRow.innerHTML = `
-                <td>${date}</td>
-                <td>${amount}</td>
-                <td>${catagory}</td>
-                <td>${type}</td>
-            `;
-    table.appendChild(newRow);
 
 
     document.getElementById("add-date").value = '';
@@ -41,7 +35,10 @@ function addTransactions() {
     document.getElementById("type").value = 'select';
 
 
+    displayTransactions();
     updateSummary();
+    renderChart();
+    renderPieChart();
 }
 
 function updateSummary() {
@@ -65,4 +62,179 @@ function updateSummary() {
     console.log(balance);
     console.log(expense);
     console.log(income);
+};
+
+function displayTransactions(data = transactions) {
+    let table = document.querySelector("tbody");
+    table.innerHTML = "";
+
+    data.forEach(t => {
+        let row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${t.Date}</td>
+            <td>${t.Amount}</td>
+            <td>${t.catagory}</td>
+            <td>${t.Type}</td>
+        `;
+        table.appendChild(row);
+    });
 }
+
+
+document.getElementById("search").addEventListener("input", e => {
+  const value = e.target.value.toLowerCase();
+
+  const filtered = transactions.filter(t =>
+    t.catagory && t.catagory.toLowerCase().includes(value)
+  );
+
+  displayTransactions(filtered);
+});
+
+
+
+document.getElementById("filterType").addEventListener("change", e => {
+  const value = e.target.value;
+
+  let filtered;
+
+  if (value === "all") {
+    filtered = transactions;
+  } else {
+    filtered = transactions.filter(t => 
+      t.Type.toLowerCase() === value.toLowerCase()
+    );
+  }
+
+  displayTransactions(filtered);
+});
+
+
+
+
+document.getElementById("sortAmount").addEventListener("change", e => {
+  let sorted = [...transactions];
+
+  if (e.target.value === "low") {
+    sorted.sort((a, b) => a.Amount - b.Amount);
+  } else if (e.target.value === "high") {
+    sorted.sort((a, b) => b.Amount - a.Amount);
+  }
+
+  displayTransactions(sorted);
+});
+
+
+
+
+
+function renderChart() {
+    const ctx = document.getElementById('myChart');
+
+    if (!ctx) {
+        console.log("Chart canvas not found");
+        return;
+    }
+
+    let dates = [];
+    let incomeData = [];
+    let expenseData = [];
+
+    transactions.forEach(t => {
+        dates.push(t.Date);
+
+        if (t.Type === "Income") {
+            incomeData.push(t.Amount);
+            expenseData.push(0);
+        } else {
+            expenseData.push(t.Amount);
+            incomeData.push(0);
+        }
+    });
+
+    if (myChart) {
+    myChart.destroy();
+    myChart = null;
+}
+
+
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dates,
+            datasets: [
+                {
+                    label: 'Income',
+                    data: incomeData,
+                    borderColor: '#22C55E',
+                    tension: 0.4
+                },
+                {
+                    label: 'Expense',
+                    data: expenseData,
+                    borderColor: '#EF4444',
+                    tension: 0.4
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+        }
+    });
+}
+
+let pieChart;
+
+function renderPieChart() {
+    const ctx = document.getElementById('pieChart');
+
+    if (!ctx) return;
+
+    let catagoryMap = {};
+
+    transactions.forEach(t => {
+        if (!catagoryMap[t.catagory]) {
+            catagoryMap[t.catagory] = 0;
+        }
+        catagoryMap[t.catagory] += t.Amount;
+    });
+
+    const labels = Object.keys(catagoryMap);
+    const data = Object.values(catagoryMap);
+
+    if (pieChart) {
+        pieChart.destroy();
+    }
+
+    pieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: [
+                    '#22C55E',
+                    '#EF4444',
+                    '#3B82F6',
+                    '#F59E0B',
+                    '#A855F7',
+                    '#14B8A6'
+                ]
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+}
+
+
+
+
+
+window.onload = function () {
+    displayTransactions();
+    updateSummary();
+    renderChart();
+    renderPieChart();
+};
